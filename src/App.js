623ae2409f02ecb9238a86a18ff7e79c41f0f836
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-27 09:50:05 -0600

    added app content so it doesnt break on initialization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 
 
+export default class App extends Component {
+
+
+
+render(){
+
+return(
+    <h1>Bangazon!</h1>
+)
+
+
+}
+
+
+
+}
