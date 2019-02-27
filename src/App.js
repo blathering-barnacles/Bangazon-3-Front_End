311c35db1c0a1206cc034db8c3b240ff1ffceb89
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-27 14:54:09 -0600

    edit working on training programs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
 import React, { Component } from 'react';
+import TrainingProgram from './components/TrainingProgramComponent/index'
 // import './App.css';
 
 
 export default class App extends Component {
 
 
+  state = {
+    trainingPrograms: [],
+    apiUrl: "http://localhost:8000/api/v1/"
+  }
+
+
+  getTrainingPrograms = (resource, keyword=null) => {
+    let url = `${this.state.apiUrl}${resource}/`
+    if(keyword) {
+      url+=keyword
+    }
+    fetch(url)
+    .then( response => response.json() )
+    .then( data => {
+      console.log("TRAINING PROGRAMS: ", data)
+      this.setState({
+        [resource]: data
+      })
+    })
+    .catch(err => console.log("Oopsy Daisy!", err))
+  }
+
+
+  create = (resource, newObj) => {
+    let formData = new FormData()
+    for ( let key in newObj ) {
+      formData.append(key, newObj[key])
+    }
+
+    fetch(`${this.state.apiUrl}${resource}/`, {
+      method: 'POST',
+      body: formData
+    })
+    .then( newData => newData.json())
+    .then( newData => {
+      console.log("Added?", newData)
+      this.getTrainingPrograms(resource)
+    })
+  }
+
+
+  edit = (resource, newObj, id) => {
+
+    let formData = new FormData()
+    for ( let key in newObj ) {
+      formData.append(key, newObj[key])
+    }
+
+    fetch(`${this.state.apiUrl}${resource}/${id}/`, {
+      method: 'PATCH',
+      body: formData
+    })
+    .then( newData => newData.json())
+    .then(() => this.getTrainingPrograms(resource))
+  }
+
 
 render(){
 
 return(
+    <div>
     <h1>Bangazon!</h1>
+    <TrainingProgram
+    getTrainingPrograms={this.getTrainingPrograms}
+    trainingPrograms={this.state.trainingPrograms}
+    create={this.create}
+    edit={this.edit}
+    />
+    </div>
 )
 
 
---
+++ src/components/TrainingProgramComponent/editProgramForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+
+export default class EditProgramForm extends Component {
+
+
+  state = {
+    name: this.props.selectedProgram.name,
+    startDate: this.props.selectedProgram.startDate,
+    endDate: this.props.selectedProgram.endDate,
+    maxAttendees: this.props.selectedProgram.maxAttendees,
+    deletedOn: ""
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.selectedProgram.name !== prevProps.selectedProgram.name){
+      console.log("PREV PROPS: ",prevProps)
+      this.setState({
+        name: this.props.selectedProgram.name,
+        startDate: this.props.selectedProgram.startDate,
+        endDate: this.props.selectedProgram.endDate,
+        maxAttendees: this.props.selectedProgram.maxAttendees
+      })
+    }
+    // console.log("PROPS", this.props.selectedProgram.name)
+    // this.props.updateState()
+  }
+
+
+
+
+  handleFieldChange = (event) => {
+    const stateToChange = {}
+    stateToChange[event.target.id] = event.target.value
+    this.setState(stateToChange)
+    console.log(stateToChange)
+  }
+
+  editProgram = (resource, newObj, id) => {
+    this.props.edit(resource, newObj, id)
+  }
+
+  closeForm = () => {
+    this.props.formDestroyer()
+  }
+
+  consoleLog = () => {
+    // console.log(this.state.name)
+    // console.log(this.state.startDate)
+    // console.log(this.state.endDate)
+    // console.log(this.state.maxAttendees)
+    console.log("SELECTED PROGRAM NAME", this.props.selectedProgram.name)
+    console.log('SELECTED PROGRAM ID', this.props.selectedProgram.id)
+  }
+
+
+render() {
+
+  return (
+    <React.Fragment>
+      <h2>Edit This Program <button onClick={this.closeForm}>Close Form</button></h2>
+      <input type="text" id="name" onChange={this.handleFieldChange} value={this.state.name}></input>
+      <input type="date" id="startDate" onChange={this.handleFieldChange} value={this.state.startDate}></input>
+      <input type="date" id="endDate"  onChange={this.handleFieldChange} value={this.state.endDate}></input>
+      <input type="text" id="maxAttendees"  onChange={this.handleFieldChange} value={this.state.maxAttendees}></input>
+      <button onClick={() => {this.editProgram("trainingPrograms", this.state, this.props.selectedProgram.id)}}>Submit Changes</button>
+      {/* <button onClick={this.consoleLog}>console log</button> */}
+
+    </React.Fragment>
+  )
+
+}
+
+
+}
