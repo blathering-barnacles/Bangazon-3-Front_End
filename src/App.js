ca1b1f97c7d4daaad58f5904d96ec4a20e5d04c2
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-27 17:43:10 -0600

    attempted to resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import TrainingProgram from './components/TrainingProgramComponent/index'
-// import './App.css';
 import APIManager from './modules/APIManager';
-// import Employee from './components/employee/employee-component'
+import Employee from './components/employee/employee-component'
 
 
 export default class App extends Component {
@@ -53,7 +52,7 @@ export default class App extends Component {
     return (
       <React.Fragment>
         <h1>Bangazon!</h1>
-        {/* <Employee getAll={this.getAll} employees={this.state.employees} /> */}
+        <Employee getAll={this.getAll} employees={this.state.employees} />
         <TrainingProgram
           getTrainingPrograms={this.getTrainingPrograms}
           trainingPrograms={this.state.trainingPrograms}
