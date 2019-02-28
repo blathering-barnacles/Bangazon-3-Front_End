a95674f51269db70b9a402a1cfdfb37ae919b796
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-28 15:38:27 -0600

    fixed duplicate computers

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -13,9 +13,9 @@ export default class NavBar extends Component {
         <nav className="navBar">
           <a href="http://localhost:3000/customers"><h4>Customers</h4></a>
           <a href="http://localhost:3000/products"><h4>Products</h4></a>
-          <a href="http://localhost:3000/product-types"><h4>Computers</h4></a>
+          <a href="http://localhost:3000/product-types"><h4>Product Types</h4></a>
           <a href="http://localhost:3000/orders"><h4>Orders</h4></a>
-          <a href="http://localhost:3000/payment-types"><h4>Computers</h4></a>
+          <a href="http://localhost:3000/payment-types"><h4>Payment Types</h4></a>
           <a href="http://localhost:3000/departments"><h4>Departments</h4></a>
           <a href="http://localhost:3000/training-programs"><h4>Training Programs</h4></a>
           <a href="http://localhost:3000/employees"><h4>Employees</h4></a>
