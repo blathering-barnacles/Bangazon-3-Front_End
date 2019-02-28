85c91eb5ed6dad75caa5084ab30f8bc0c0d6d96e
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-28 15:29:09 -0600

    bare bones nav bar is good to go

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './index.css'
 
 
 export default class NavBar extends Component {
@@ -9,10 +10,12 @@ export default class NavBar extends Component {
     return (
 
       <React.Fragment>
-        <nav>
+        <nav className="navBar">
           <a href="http://localhost:3000/customers"><h4>Customers</h4></a>
           <a href="http://localhost:3000/products"><h4>Products</h4></a>
+          <a href="http://localhost:3000/product-types"><h4>Computers</h4></a>
           <a href="http://localhost:3000/orders"><h4>Orders</h4></a>
+          <a href="http://localhost:3000/payment-types"><h4>Computers</h4></a>
           <a href="http://localhost:3000/departments"><h4>Departments</h4></a>
           <a href="http://localhost:3000/training-programs"><h4>Training Programs</h4></a>
           <a href="http://localhost:3000/employees"><h4>Employees</h4></a>
